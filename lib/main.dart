aa61a0e3e08039fc85943b7bacabdb12499c9184
hayabusabusa <hayabusabusaSY@gmail.com>
2020-05-26 08:41:12 +0900

    Setup articles notifier and impl articles screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:provider_mvvm_sample/notifiers/notifiers.dart';
+import 'package:provider_mvvm_sample/screens/screens.dart';
 
 void main() {
-  
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'provider mvvm sample',
+      home: ChangeNotifierProvider<ArticlesNotifier>(
+        create: (_) => ArticlesNotifier(),
+        child: ArticlesScreen(),
+      ),
+    );
+  }
 }
---
+++ lib/screens/articles_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:provider_mvvm_sample/notifiers/notifiers.dart';
+import 'package:provider_mvvm_sample/widgets/widgets.dart';
+
+class ArticlesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Qiita', style: TextStyle(color: Colors.green, fontSize: 20, fontWeight: FontWeight.w600),),
+        brightness: Brightness.light,
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(color: Colors.green),
+      ),
+      // NOTE: Cunsummer だとその都度 context が再発行されて無限ループになるのでは.
+      body: context.select((ArticlesNotifier value) => value.isLoading
+          ? Center(
+            child: CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.green[300]),
+            )
+          )
+          : Scrollbar(
+            child: ListView.builder(
+              itemCount: value.items.length,
+              itemBuilder: (context, index) => ArticleItem(
+                qiitaItem: value.items[index], 
+                onTap: (item) { print(item); },
+              ),
+            ),
+          )
+        ),
+    );
+  }
+}

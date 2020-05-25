ca61749d7e31cead63137602be3c6151e6c2eabd
hayabusabusa <hayabusabusaSY@gmail.com>
2020-05-26 08:22:11 +0900

    Fix articles notifier

--- lib/notifiers/articles_notifier.dart
+++ lib/notifiers/articles_notifier.dart
@@ -2,13 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:provider_mvvm_sample/models/network/api_client.dart';
 import 'package:provider_mvvm_sample/models/entities/entities.dart';
 
-import '../models/entities/entities.dart';
-
 // TODO: あえて Fat な作りにするので可能ならリファクタ.
 class ArticlesNotifier with ChangeNotifier {
   final APIClient _apiClient = APIClient();
 
-  bool _isLoading = false;
+  bool _isLoading = true;
   bool get isLoading => _isLoading;
 
   void _setIsLoading(bool isLoading) {
@@ -32,11 +30,16 @@ class ArticlesNotifier with ChangeNotifier {
     notifyListeners();
   }
 
-  void fetch() async {
-    _setIsLoading(true);
+  ArticlesNotifier() {
+    // NOTE: build() メソッドなどのライフサイクルイベント上では
+    // notifyListeners() を呼べないので、
+    // コンストラクタで初回フェッチの処理を行う.
+    fetch();
+  }
 
+  void fetch() async {
     try {
-      final all = await _apiClient.getAllItems(0);
+      final all = await _apiClient.getAllItems(1);
       _setIsLoading(false);
       _setItems(all.items);
     } catch (error) {
